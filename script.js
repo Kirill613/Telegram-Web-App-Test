8d9bfc408405acf7027cbdc1ac9b9201f3431200
Kirill <kirillshcherbakov2002@gmail.com>
2024-04-27 17:23:08 +0300

    update6

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+$.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
+  data = data.trim().split('\n').reduce(function(obj, pair) {
+    pair = pair.split('=');
+    return obj[pair[0]] = pair[1], obj;
+  }, {});
+  console.log(data);
+});
+
 openBtn.addEventListener("click", () => {
     const phrases = ["Да", "Нет"];
     const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
